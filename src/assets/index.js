73fb4eb29be13076f6eb448c0f74f00c64f9cd4d
CJ-Codes <ccoreas484@gmail.com>
2023-09-07 19:55:45 -0400

    portfolio images and symbols uploaded

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import portfolio1 from './portfolio_1.png';
+import portfolio2 from './portfolio_2.png';
+import portfolio3 from './portfolio_3.png';
+import portfolio4 from './portfolio_4.png';
+import portfolio5 from './portfolio_5.png';
+import portfolio6 from './portfolio_6.png';
+import portfolio7 from './portfolio_7.png';
+import portfolio8 from './portfolio_8.png';
+
+const portfolioImages = [portfolio1, portfolio2, portfolio3, portfolio4, portfolio5, portfolio6, portfolio7,
+portfolio8];
+
+export default portfolioImages;
---
+++ src/assets/symbols/index.js
@@ -0,0 +1,24 @@
+import moon from './moon.png';
+import sun from './sun.png';
+import openOn from './Open=On.png';
+import openOff from './Open=Off.png';
+
+import left_1 from './left_1.png';
+import right_1 from './right_1.png';
+import up_1 from './up_1.png';
+
+import left_2 from './left_2.png';
+import right_2 from './right_2.png';
+import up_2 from './up_2.png';
+import down_1 from './down_1.png';
+
+import instagram from './instagram.png';
+import twitter from './twitter.png';
+import youtube from './youtube.png';
+
+import phone from './phone.png';
+import mail from './mail.png';
+
+export {moon, sun, openOff, openOn, left_1, right_1, up_1, left_2
+, right_2, up_2, down_1, instagram, twitter
+, youtube, phone, mail}

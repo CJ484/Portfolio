11827439c34ed7a64c16f8b44d970f00bedfd6fa
CJ-Codes <ccoreas484@gmail.com>
2023-09-08 12:57:50 -0400

    converted all symbols to .svg files

--- src/assets/symbols/index.js
+++ src/assets/symbols/index.js
@@ -1,23 +1,23 @@
-import moon from './moon.png';
-import sun from './sun.png';
-import openOn from './Open=On.png';
-import openOff from './Open=Off.png';
+import moon from './moon.svg';
+import sun from './sun.svg';
+import openOn from './Open=On.svg';
+import openOff from './Open=Off.svg';
 
-import left_1 from './left_1.png';
-import right_1 from './right_1.png';
-import up_1 from './up_1.png';
+import left_1 from './left_1.svg';
+import right_1 from './right_1.svg';
+import up_1 from './up_1.svg';
 
-import left_2 from './left_2.png';
-import right_2 from './right_2.png';
-import up_2 from './up_2.png';
-import down_1 from './down_1.png';
+import left_2 from './left_2.svg';
+import right_2 from './right_2.svg';
+import up_2 from './up_2.svg';
+import down_1 from './down_1.svg';
 
-import instagram from './instagram.png';
-import twitter from './twitter.png';
-import youtube from './youtube.png';
+import instagram from './instagram.svg';
+import twitter from './twitter.svg';
+import youtube from './youtube.svg';
 
-import phone from './phone.png';
-import mail from './mail.png';
+import phone from './phone.svg';
+import mail from './mail.svg';
 
 export {moon, sun, openOff, openOn, left_1, right_1, up_1, left_2
 , right_2, up_2, down_1, instagram, twitter
